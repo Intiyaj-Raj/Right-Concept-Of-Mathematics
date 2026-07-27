810947d55f48ebcb7d3cd39bce713621b3e36fc4
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-07-27 11:58:26 +0530

    change robots url

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
+      userAgent: "*",
+      allow: "/",
     },
-    sitemap: 'https://rightconceptofmathematics.com/sitemap.xml',
+    sitemap: "https://rightconceptofmathematics.vercel.app/sitemap.xml",
   };
 }
